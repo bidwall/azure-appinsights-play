25ccb169c27fe5330654fa32653a69717e9aa84a
Palvinder Bidwall <pal@bidwall.co.uk>
2018-03-18 20:33:38 +0000

    Add project files.

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static TelemetryClient _telemetryClient;
+
+        static void Main(string[] args)
+        {
+            SetupTelemetry();
+            WriteTelemetry();
+
+            //Console.ReadKey();
+        }
+
+        static void SetupTelemetry()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+
+            var instrumentationKey = builder.Build()["instrumentationKey"];
+
+            TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
+
+            _telemetryClient = new TelemetryClient();
+        }
+
+        static void WriteTelemetry()
+        {
+            WriteTrace("message");
+            WriteEvent("event");
+            WriteException(new Exception("Exception"));
+
+            _telemetryClient.Flush();       // more efficient not to flush, but ok for playing ;-)
+        }
+
+        static void WriteTrace(string message)
+        {
+            TelemetryLogger.LogTrace(message);
+        }
+
+        static void WriteEvent(string eventName)
+        {
+            TelemetryLogger.LogEvent(eventName);
+        }
+
+        static void WriteException(Exception exception)
+        {
+            TelemetryLogger.LogException(exception);
+        }
+    }
+}
---
+++ src/ConsoleApp/TelemetryLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.ApplicationInsights;
+
+namespace ConsoleApp
+{
+    public static class TelemetryLogger
+    {
+        private static readonly TelemetryClient TelemetryClient;
+
+        static TelemetryLogger()
+        {
+            TelemetryClient = new TelemetryClient();
+        }
+
+        public static void LogTrace(string message, [CallerFilePath]string filePath = "", [CallerMemberName] string memberName = "", [CallerLineNumber]int lineNumber = 0)
+        {
+            TelemetryClient.TrackTrace(message, CreateProperties(filePath, memberName, lineNumber));
+        }
+
+        public static void LogEvent(string eventName)
+        {
+            TelemetryClient.TrackEvent(eventName);
+        }
+
+        public static void LogException(Exception exception)
+        {
+            TelemetryClient.TrackException(exception);
+        }
+
+        private static IDictionary<string, string> CreateProperties(string filePath, string memberName, int lineNumber)
+        {
+            return new Dictionary<string, string>
+            {
+                {"File Path", filePath},
+                {"Member Name", memberName},
+                {"Line Number", lineNumber.ToString()}
+            };
+        }
+    }
+}
